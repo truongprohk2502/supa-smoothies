14749654e879aff4fb47dbfa7601e0b243f46052
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-15 08:10:06 +0700

    Init project

---
+++ src/pages/Create.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="page home">
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Update.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Create: FC = () => {
+  return (
+    <div className="page create">
+      <h2>Create</h2>
+    </div>
+  );
+};
+
+export default Create;
