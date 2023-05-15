6a7205fcb1223e7fadbf909e543ef8e57f468be7
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-15 08:21:32 +0700

    Install supabase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
+import supabase from "../config/supabaseClient";
 
 const Home: FC = () => {
+  console.log(supabase);
+
   return (
     <div className="page home">
       <h2>Home</h2>
